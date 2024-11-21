0f522cc1bdecbcd7556569097f73087dd47092ba
Aditya Bavadekar <bvkraditya@gmail.com>
2024-11-22 01:10:31 +0530

    Updated Hospital dashboard

--- client/src/components/dashboardHospital.jsx
+++ client/src/components/dashboardHospital.jsx
@@ -1,7 +1,30 @@
 import { Search, CalendarCheck2, Pen, NotepadText, Hospital } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+import ApiConstants from '../constants/ApiConstants';
 
 const DashboardHospital = () => {
+    const [hospital, setHospital] = useState({});
+
+    useEffect(() => {
+        const token = Cookies.get('jwt');
+        fetch(ApiConstants.API_ME, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setHospital(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }, [])
+    
     return (
         <div className="p-2">
             <h1 className="text-3xl font-bold text-center mb-8 text-gray-800 py-5">Hospital Dashboard</h1>
@@ -10,9 +33,21 @@ const DashboardHospital = () => {
             <div className="bg-gray-100 p-6 rounded-lg shadow-md mb-6 mx-6 gap-4 flex flex-col items-center justify-center h-full">
                     <Hospital height={'62px'} width={'auto'} className='text-gray-700'/>
                     <div className="text-center">
-                        <h2 className="text-3xl font-bold text-gray-800 mb-3">City Hospital</h2>
-                        <p className="text-sm text-gray-600">123 Main Street, Springfield</p>
-                        <p className="text-sm text-gray-600">Contact: (555) 123-4567</p>
+                        <h2 className="text-3xl font-bold text-gray-800 mb-3">
+                            {
+                                hospital.name ? hospital.name : 'Hospital Name'
+                            }
+                        </h2>
+                        <p className="text-sm text-gray-600">
+                            {
+                                hospital.address ? hospital.address : 'Hospital Address'
+                            }
+                        </p>
+                        <p className="text-sm text-gray-600">Contact: 
+                            {
+                                hospital.mobileNumber ? ` ${hospital.mobileNumber}` : ' Hospital Contact'
+                            }
+                        </p>
                     </div>
                 </div>
 
