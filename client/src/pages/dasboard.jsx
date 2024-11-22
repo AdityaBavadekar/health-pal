2ff8b9664cfc9066781883ce632a09a1fc915d18
dhruvl2006 <lohar.dhruv07@gmail.com>
2024-11-22 08:48:40 +0530

    Dashboard changes

--- client/src/pages/dasboard.jsx
+++ client/src/pages/dasboard.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import DashboardHospital from "../components/dashboardHospital";
 import DashboardDoctor from "../components/dashboardDoctor";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 const Dashboard = () => {
+  const token = Cookies.get("jwt");
+
+  const user = jwtDecode(token);
+  const [userType, setUserType] = useState(user.type);
+  console.log(userType);
   return (
     <div className="px-5 py-3">
-      <DashboardHospital />
-      {/* <DashboardDoctor /> */}
+      {userType === "Hospital" ? (
+        <DashboardHospital />
+      ) : userType == "Doctor" ? (
+        <DashboardDoctor />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
